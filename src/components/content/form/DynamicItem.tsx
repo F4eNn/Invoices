6dd1d5a848c6c7a2f20145f1f18c29ef127edd3f
Mateusz <mateusz4k@outlook.com>
2023-08-23 15:13:19 +0200

    refactor: move the creation of dynamic inputs to a separate component

--- src/components/content/form/DynamicItem.tsx
+++ src/components/content/form/DynamicItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Control, FieldErrors } from 'react-hook-form'
+import { Control, FieldArrayWithId, FieldErrors, UseFieldArrayRemove } from 'react-hook-form'
 
 import { DeleteIcon } from '@/components/icons/Delete'
 import { ControlInput } from './ControlInput'
@@ -8,28 +8,60 @@ import { InvoiceFormValues } from './InvoiceForm'
 interface DynamicItemProps {
 	control: Control<InvoiceFormValues>
 	error: FieldErrors<InvoiceFormValues>
+	fields: FieldArrayWithId<InvoiceFormValues, 'items', 'id'>[]
+	remove: UseFieldArrayRemove
 }
 
-export const DynamicItem = ({ control, error }: DynamicItemProps) => {
+export const DynamicItem = ({ control, error, fields, remove }: DynamicItemProps) => {
 	return (
 		<>
-			<tr className='mb-3 flex items-center justify-between gap-4'>
-				<td className='w-full'>
-					{/* <ControlInput control={control} error={error.}/> */}
-					<input type='text' className='invoice-input form-input' />
-				</td>
-				<td className='w-2/12'>{/* <ControlInput /> */}</td>
-				<td>
-					{/* <ControlInput /> */}
-					<input type='text' className='invoice-input form-input' />
-				</td>
-				<td>186.25</td>
-				<td>
-					<button type='button' className='group p-3 '>
-						<DeleteIcon />
-					</button>
-				</td>
-			</tr>
+			{fields.map((field, idx) => (
+				<div key={field.id} className='mb-3 flex flex-col justify-center gap-4 lg:flex-row lg:items-center'>
+					<div className='mt-10 w-full lg:mt-0'>
+						<span className='lg:hidden'>Item name</span>
+						<ControlInput
+							control={control}
+							error={error.items?.[idx]?.name}
+							id={`name${idx}`}
+							name={`items.${idx}.name`}
+							items={true}
+						/>
+					</div>
+					<div className='flex items-center gap-4'>
+						<div className='w-5/12 '>
+							<span className='lg:hidden'>Qty.</span>
+							<ControlInput
+								type='number'
+								control={control}
+								error={error.items?.[idx]?.quantity}
+								id={`quantity${idx}`}
+								name={`items.${idx}.quantity`}
+								items={true}
+							/>
+						</div>
+						<div>
+							<span className='lg:hidden'>Price</span>
+							<ControlInput
+								control={control}
+								type='number'
+								error={error.items?.[idx]?.price}
+								id={`price${idx}`}
+								name={`items.${idx}.price`}
+								items={true}
+							/>
+						</div>
+						<div className=' relative flex h-full flex-col '>
+							<span className='absolute -top-3.5 left-1 lg:hidden '>Total</span>
+							<span className='mt-8'>186.54</span>
+						</div>
+						<div>
+							<button type='button' onClick={() => remove(idx)} className='group p-3 '>
+								<DeleteIcon />
+							</button>
+						</div>
+					</div>
+				</div>
+			))}
 		</>
 	)
 }
--- src/components/content/form/ItemList.tsx
+++ src/components/content/form/ItemList.tsx
@@ -3,8 +3,7 @@ import { Control, FieldErrors, useFieldArray } from 'react-hook-form'
 
 import { Button } from '@/components/ui/Button'
 import { InvoiceFormValues } from './InvoiceForm'
-import { DeleteIcon } from '@/components/icons/Delete'
-import { ControlInput } from './ControlInput'
+import { DynamicItem } from './DynamicItem'
 
 interface ItemList {
 	control: Control<InvoiceFormValues>
@@ -29,58 +28,10 @@ export const ItemListForm = ({ control, error }: ItemList) => {
 					<span className=''></span>
 				</div>
 				<div>
-					{fields.map((field, idx) => (
-						<div key={field.id} className='mb-3 flex flex-col justify-center gap-4 lg:flex-row lg:items-center'>
-							<div className='mt-10 w-full lg:mt-0'>
-								<span className='lg:hidden'>Item name</span>
-								<ControlInput
-									control={control}
-									error={error.items?.[idx]?.name}
-									id={`name${idx}`}
-									name={`items.${idx}.name`}
-									items={true}
-								/>
-							</div>
-							<div className='flex items-center gap-4'>
-								<div className='w-5/12 '>
-									<span className='lg:hidden'>Qty.</span>
-									<ControlInput
-										type='number'
-										control={control}
-										error={error.items?.[idx]?.quantity}
-										id={`quantity${idx}`}
-										name={`items.${idx}.quantity`}
-										items={true}
-									/>
-								</div>
-								<div>
-									<span className='lg:hidden'>Price</span>
-
-									<ControlInput
-										control={control}
-										type='number'
-										error={error.items?.[idx]?.price}
-										id={`price${idx}`}
-										name={`items.${idx}.price`}
-										items={true}
-									/>
-								</div>
-								<div className=' relative flex h-full flex-col '>
-									<span className='absolute -top-3.5 left-1 lg:hidden '>Total</span>
-									<span className='mt-8'>186.54</span>
-								</div>
-								<div>
-									<button type='button' onClick={() => remove(idx)} className='group p-3 '>
-										<DeleteIcon />
-									</button>
-								</div>
-							</div>
-						</div>
-					))}
+					<DynamicItem control={control} error={error} remove={remove} fields={fields} />
 				</div>
 			</div>
-
-			<div className='mt-3 overflow-hidden rounded-3xl bg-secondaryDark text-white hover:bg-darkGray'>
+			<div className='mt-3 overflow-hidden rounded-3xl bg-secondaryDark  text-white hover:bg-darkGray'>
 				<Button
 					onClick={() =>
 						append({
